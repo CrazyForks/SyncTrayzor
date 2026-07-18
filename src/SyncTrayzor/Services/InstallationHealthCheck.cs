78e98bbfe46bce7337ded330432cd4dccc71d3bb
GermanCoding <github@germancoding.com>
2026-07-18 12:59:19 +0200

    fix(InstallationHealthCheck): Clarify remediation steps when mixed files are detected

--- src/SyncTrayzor/Services/InstallationHealthCheck.cs
+++ src/SyncTrayzor/Services/InstallationHealthCheck.cs
@@ -18,7 +18,11 @@ public class InstallationHealthCheck(IWindowManager windowManager) : IInstallati
         if (File.Exists(v1File))
         {
             windowManager.ShowMessageBox(
-                "It looks like this SyncTrayzor installation contains files from SyncTrayzor v1. This will likely cause SyncTrayzor to crash. Please uninstall and re-install SyncTrayzor to remove v1 installation files. If you are running the portable version, you can move the \"data\" folder from your existing install to keep user data.",
+                "It looks like this SyncTrayzor installation contains files from SyncTrayzor v1. This may cause SyncTrayzor to crash.\n" +
+                $"Please uninstall SyncTrayzor, and then manually clean the directory {programDirectory} to remove v1 installation files.\n" +
+                $"Afterwards, you can re-install SyncTrayzor normally.\n" +
+                $"If you are running the portable version, backup and restore the \"data\" folder from your existing install to keep user data.\n" +
+                $"The installed version keeps user data by default when uninstalling.",
                 "Mixed v1/v2 Installation Detected", System.Windows.MessageBoxButton.OK,
                 System.Windows.MessageBoxImage.Warning);
         }
